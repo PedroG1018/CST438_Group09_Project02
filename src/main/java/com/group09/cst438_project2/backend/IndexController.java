8d75d25036629633649ef771e391a3163744a45d
bryan <bfowles@csumb.edu>
2022-03-02 22:28:44 -0800

    made basic templates for our pages, they are connected to the controller

--- src/main/java/com/group09/cst438_project2/backend/IndexController.java
+++ src/main/java/com/group09/cst438_project2/backend/IndexController.java
@@ -10,6 +10,26 @@ public class IndexController {
     public String index(Model model) {
         model.addAttribute("message", "Thank you for visiting.");
 
-        return "index";
+        return "home";
+    }
+
+    @GetMapping("/registerUserPage")
+    public String registerUserPage(Model model) {
+        return "registerUserPage";
+    }
+
+    @GetMapping("/loginPage")
+    public String loginPage(Model model) {
+        return "loginPage";
+    }
+
+    @GetMapping("/editItemPage")
+    public String editItemPage(Model model) {
+        return "editItemPage";
+    }
+
+    @GetMapping("/listPage")
+    public String listPage(Model model) {
+        return "listPage";
     }
 }
